1468b47f1714fa947bd1b388ee667ecf38b80053
sammanthp007 <samman.thapa@bison.howard.edu>
2017-02-13 03:40:19 -0500

    initialize BusinessViewCell class for the cell for the main storyboard's cell

---
+++ Yelp/BusinessViewCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BusinessViewCellTableViewCell.swift
+//  Yelp
+//
+//  Created by samman on 2/13/17.
+//  Copyright © 2017 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessViewCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
