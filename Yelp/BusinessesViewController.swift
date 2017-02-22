68d81e07fa74534d897e5de1f788310a0557ca20
samman ios <sammanios@Sudheeshs-MacBook-Pro.local>
2017-02-21 19:43:48 -0500

    businessViewController add searchbar

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,17 +8,22 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class BusinessesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     
     var businesses: [Business]!
+    // for search bar
+    var filteredBusiness : [Business]!
+    
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
+        searchBar.delegate = self
         
         // make sure that tableview follows the autolayout constraints
         tableView.rowHeight = UITableViewAutomaticDimension
@@ -29,6 +34,9 @@ class BusinessesViewController: UIViewController, UITableViewDelegate, UITableVi
         Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
             
             self.businesses = businesses
+            
+            self.filteredBusiness = self.businesses
+            
             self.tableView.reloadData()
             if let businesses = businesses {
                 for business in businesses {
@@ -60,18 +68,19 @@ class BusinessesViewController: UIViewController, UITableViewDelegate, UITableVi
     
     // how many cells
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if businesses != nil {
-            return businesses.count
-        } else {
+            if let filtered_businesses = self.filteredBusiness {
+                return filtered_businesses.count
+            }
             return 0
         }
-    }
     
     // for each cell in the table
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath) as! BusinessViewCellTableViewCell
         
-        cell.business = self.businesses[indexPath.row]
+        //cell.business = self.businesses[indexPath.row]
+        
+        cell.business = self.filteredBusiness[indexPath.row]
         return cell
     }
     
@@ -84,5 +93,38 @@ class BusinessesViewController: UIViewController, UITableViewDelegate, UITableVi
      // Pass the selected object to the new view controller.
      }
      */
+    
+    // Search Bar
+    // This method updates filteredData based on the text in the Search Box
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // When there is no text, filteredData is the same as the original data
+        // When user has entered text into the search box
+        // Use the filter method to iterate over all items in the data array
+        // For each item, return true if the item should be included and false if the
+        // item should NOT be included
+        self.filteredBusiness = searchText.isEmpty ? businesses : businesses?.filter({(dataDict: Business) -> Bool in
+            // If dataItem matches the searchText, return true to include it
+            let title = dataDict.name! as String
+            
+            // return true if searchText matches title
+            return title.range(of: searchText, options: .caseInsensitive) != nil
+        })
+        
+        tableView.reloadData()
+    }
+    
+    // When search starts
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        self.searchBar.showsCancelButton = true
+    }
+    
+    // when cancel button clicked
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        self.filteredBusiness = self.businesses
+        searchBar.resignFirstResponder()
+        self.tableView.reloadData()
+    }
     
 }
